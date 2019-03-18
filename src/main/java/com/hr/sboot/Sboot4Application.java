ecb840762a4ccdab310a7aed0a224a91ecdefbae
jylei-admin <1619052660@qq.cpm>
2019-03-18 11:23:34 +0800

    第一次提交

---
+++ src/main/java/com/hr/sboot/Sboot4Application.java
@@ -0,0 +1,13 @@
+package com.hr.sboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sboot4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sboot4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hr/sboot/Sboot4ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hr.sboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Sboot4ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
